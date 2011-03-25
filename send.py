9b3be0ac9bf8cafe4e212f36faf73e4c89bf012d
Luke Dashjr <luke-jr+git@utopios.org>
2011-03-25 11:25:06 -0400

    GPLv3 copying terms; genjix has assigned copyright to me

--- send.py
+++ send.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+# Spesmilo -- Python Bitcoin user interface
+# Copyright © 2011 Luke Dashjr
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 only.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from decimal import Decimal
 import re
 from PySide.QtCore import *
