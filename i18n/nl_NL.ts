86c914384b96a41eb7d584aba2b1f53168ba505e
Jos van Hees <jos.vanhees@gmail.com>
2011-04-08 17:44:10 -0400

    nl_NL translation

--- i18n/nl_NL.ts
+++ i18n/nl_NL.ts
@@ -1,61 +1,62 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="nl_NL">
 <context>
     <name>Cashier</name>
     <message>
         <location filename="cashier.py" line="244"/>
         <source>Your address:</source>
-        <translation type="unfinished"></translation>
+        <translation>Uw adres:</translation>
     </message>
     <message>
         <location filename="cashier.py" line="277"/>
         <source>Spesmilo</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="cashier.py" line="454"/>
         <source>Balance: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Saldo: %s</translation>
     </message>
     <message>
         <location filename="cashier.py" line="458"/>
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>Verstuur</translation>
     </message>
     <message>
         <location filename="cashier.py" line="458"/>
         <source>Send bitcoins to another person</source>
-        <translation type="unfinished"></translation>
+        <translation>Verstuur bitcoins naar een andere persoon</translation>
     </message>
     <message>
         <location filename="cashier.py" line="461"/>
         <source>New address</source>
-        <translation type="unfinished"></translation>
+        <translation>Nieuw adres</translation>
     </message>
     <message>
         <location filename="cashier.py" line="461"/>
         <source>Create new address for accepting bitcoins</source>
-        <translation type="unfinished"></translation>
+        <translation>Genereer een nieuw adres om bitcoins op te ontvangen</translation>
     </message>
     <message>
         <location filename="cashier.py" line="465"/>
         <source>Copy address</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieer adres</translation>
     </message>
     <message>
         <location filename="cashier.py" line="465"/>
         <source>Copy address to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Kopieer naar plakbord</translation>
     </message>
     <message>
         <location filename="cashier.py" line="469"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Opties</translation>
     </message>
     <message>
         <location filename="cashier.py" line="469"/>
         <source>Configure Spesmilo</source>
-        <translation type="unfinished"></translation>
+        <translation>Configureer Spesmilo</translation>
     </message>
 </context>
 <context>
@@ -63,7 +64,7 @@
     <message>
         <location filename="main.py" line="56"/>
         <source>Connecting...</source>
-        <translation type="unfinished"></translation>
+        <translation>Verbinden...</translation>
     </message>
 </context>
 <context>
@@ -71,7 +72,7 @@
     <message>
         <location filename="main.py" line="186"/>
         <source>Error connecting</source>
-        <translation type="unfinished"></translation>
+        <translation>Error bij verbinden</translation>
     </message>
 </context>
 <context>
@@ -79,72 +80,72 @@
     <message>
         <location filename="send.py" line="42"/>
         <source>BTC</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="send.py" line="43"/>
         <source>TBC</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="send.py" line="47"/>
         <source>Pay to:</source>
-        <translation type="unfinished"></translation>
+        <translation>Betalen aan:</translation>
     </message>
     <message>
         <location filename="send.py" line="52"/>
         <source>Amount:</source>
-        <translation type="unfinished"></translation>
+        <translation>Hoeveelheid:</translation>
     </message>
     <message>
         <location filename="send.py" line="55"/>
         <source>&amp;Send</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Verstuur</translation>
     </message>
     <message>
         <location filename="send.py" line="58"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Annuleer</translation>
     </message>
     <message>
         <location filename="send.py" line="65"/>
         <source>&lt;i&gt;Enter a bitcoin address (e.g. 1A9Pv2PYuZYvfqku7sJxovw99Az72mZ4YH)&lt;/i&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;i&gt;Geef een bitcoin adres (bijv. 1A9Pv2PYuZYvfqku7sJxovw99Az72mZ4YH)&lt;/i&gt;</translation>
     </message>
     <message>
         <location filename="send.py" line="73"/>
         <source>Send bitcoins</source>
-        <translation type="unfinished"></translation>
+        <translation>Verstuur bitcoins</translation>
     </message>
     <message>
         <location filename="send.py" line="152"/>
         <source>Invalid address</source>
-        <translation type="unfinished"></translation>
+        <translation>Ongeldig adres</translation>
     </message>
     <message>
         <location filename="send.py" line="152"/>
         <source>Invalid address: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Ongeldig adres: %s</translation>
     </message>
     <message>
         <location filename="send.py" line="165"/>
         <source>Insufficient balance</source>
-        <translation type="unfinished"></translation>
+        <translation>Onvoldoende saldo</translation>
     </message>
     <message>
         <location filename="send.py" line="165"/>
         <source>Balance of %g is too small.</source>
-        <translation type="unfinished"></translation>
+        <translation>Saldo van %g is onvoldoende.</translation>
     </message>
     <message>
         <location filename="send.py" line="175"/>
         <source>Error sending</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout bij versturen</translation>
     </message>
     <message>
         <location filename="send.py" line="175"/>
         <source>Your send failed: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Fout bij versturen: %s</translation>
     </message>
 </context>
 <context>
@@ -152,32 +153,32 @@
     <message>
         <location filename="settings.py" line="236"/>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="settings.py" line="241"/>
         <source>&amp;Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Toepassen</translation>
     </message>
     <message>
         <location filename="settings.py" line="246"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Annuleer</translation>
     </message>
     <message>
         <location filename="settings.py" line="258"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Opties</translation>
     </message>
     <message>
         <location filename="settings.py" line="283"/>
         <source>Restart required</source>
-        <translation type="unfinished"></translation>
+        <translation>Herstart vereist</translation>
     </message>
     <message>
         <location filename="settings.py" line="283"/>
         <source>Restarting Spesmilo is required for some changes to take effect.</source>
-        <translation type="unfinished"></translation>
+        <translation>Het herstarten van Spesmilo is vereist om de instellingen toe te passen.</translation>
     </message>
 </context>
 <context>
@@ -185,12 +186,12 @@
     <message>
         <location filename="settings.py" line="139"/>
         <source>Use internal core</source>
-        <translation type="unfinished"></translation>
+        <translation>Draai zelfstandig</translation>
     </message>
     <message>
         <location filename="settings.py" line="144"/>
         <source>URI:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -198,62 +199,62 @@
     <message>
         <location filename="settings.py" line="171"/>
         <source>American</source>
-        <translation type="unfinished"></translation>
+        <translation>Amerikaans</translation>
     </message>
     <message>
         <location filename="settings.py" line="172"/>
         <source>English</source>
-        <translation type="unfinished"></translation>
+        <translation>Engels</translation>
     </message>
     <message>
         <location filename="settings.py" line="173"/>
         <source>Esperanto</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="settings.py" line="176"/>
         <source>Language:</source>
-        <translation type="unfinished"></translation>
+        <translation>Taal:</translation>
     </message>
     <message>
         <location filename="settings.py" line="181"/>
         <source>Assume</source>
-        <translation type="unfinished"></translation>
+        <translation>Aannemen</translation>
     </message>
     <message>
         <location filename="settings.py" line="182"/>
         <source>Prefer</source>
-        <translation type="unfinished"></translation>
+        <translation>Voorkeur</translation>
     </message>
     <message>
         <location filename="settings.py" line="183"/>
         <source>Force</source>
-        <translation type="unfinished"></translation>
+        <translation>Forceer</translation>
     </message>
     <message>
         <location filename="settings.py" line="188"/>
         <source>Decimal</source>
-        <translation type="unfinished"></translation>
+        <translation>Decimaal</translation>
     </message>
     <message>
         <location filename="settings.py" line="189"/>
         <source>Tonal</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tonaal</translation>
     </message>
     <message>
         <location filename="settings.py" line="192"/>
         <source>Number system:</source>
-        <translation type="unfinished"></translation>
+        <translation>Talstelsel:</translation>
     </message>
     <message>
         <location filename="settings.py" line="194"/>
         <source>Hide preferred unit name</source>
-        <translation type="unfinished"></translation>
+        <translation>Verberg eenheid</translation>
     </message>
     <message>
         <location filename="settings.py" line="174"/>
         <source>Dutch</source>
-        <translation type="unfinished"></translation>
+        <translation>Nederlands</translation>
     </message>
 </context>
 <context>
@@ -261,62 +262,62 @@
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum</translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Transactions</source>
-        <translation type="unfinished"></translation>
+        <translation>Transacties</translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Credits</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Balance</source>
-        <translation type="unfinished"></translation>
+        <translation>Saldo</translation>
     </message>
     <message>
         <location filename="cashier.py" line="109"/>
         <source>Confirmed (%s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Bevestigd (%s)</translation>
     </message>
     <message>
         <location filename="cashier.py" line="111"/>
         <source>Processing... (%s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Verwerken... (%s)</translation>
     </message>
     <message>
         <location filename="cashier.py" line="113"/>
         <source>Validating... (%s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Controleren... (%s)</translation>
     </message>
     <message>
         <location filename="cashier.py" line="159"/>
         <source>Sent to %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Verzonden aan %s</translation>
     </message>
     <message>
         <location filename="cashier.py" line="161"/>
         <source>Received to %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Ontvangen van %s</translation>
     </message>
     <message>
         <location filename="cashier.py" line="163"/>
         <source>Generated</source>
-        <translation type="unfinished"></translation>
+        <translation>Gegenereerd</translation>
     </message>
     <message>
         <location filename="cashier.py" line="165"/>
         <source>Moved</source>
-        <translation type="unfinished"></translation>
+        <translation>Verplaatst</translation>
     </message>
 </context>
 <context>
@@ -324,17 +325,17 @@
     <message>
         <location filename="main.py" line="85"/>
         <source>&amp;Cashier</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Kashouder</translation>
     </message>
     <message>
         <location filename="main.py" line="89"/>
         <source>&amp;Send funds</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Verstuur geld</translation>
     </message>
     <message>
         <location filename="main.py" line="94"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Afsluiten</translation>
     </message>
 </context>
 <context>
@@ -342,27 +343,27 @@
     <message>
         <location filename="main.py" line="242"/>
         <source>Usage: %prog [options] [URI]</source>
-        <translation type="unfinished"></translation>
+        <translation>Commandoregel: %prog [opties] [URI]</translation>
     </message>
     <message>
         <location filename="main.py" line="244"/>
         <source>Use this caption for the cashier window</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik dit onderschrift voor het kashouderscherm</translation>
     </message>
     <message>
         <location filename="main.py" line="250"/>
         <source>Opens an interactive Python prompt, and enables infinite in-RAM logging</source>
-        <translation type="unfinished"></translation>
+        <translation>Opent een interactieve Python prompt en zet oneindig RAM loggen aan</translation>
     </message>
     <message>
         <location filename="main.py" line="252"/>
         <source>Use this window icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Gebruik dit icoon</translation>
     </message>
     <message>
         <location filename="main.py" line="254"/>
         <source>Opens a dialog to send funds</source>
-        <translation type="unfinished"></translation>
+        <translation>Opent een scherm om te betalen</translation>
     </message>
 </context>
 </TS>
