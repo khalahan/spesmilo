a0e95ca6fd47acc3e0c14555c56c1edb919ce87c
bitcoiner <admin@isnan.org>
2011-04-11 16:44:04 -0400

    fr_CA translation, draft 1

--- i18n/fr_CA.ts
+++ i18n/fr_CA.ts
@@ -1,61 +1,61 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="1.1" language="fr_CA">
 <context>
     <name>Cashier</name>
     <message>
         <location filename="cashier.py" line="244"/>
         <source>Your address:</source>
-        <translation type="unfinished"></translation>
+        <translation>Votre Adresse:</translation>
     </message>
     <message>
         <location filename="cashier.py" line="277"/>
         <source>Spesmilo</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="cashier.py" line="454"/>
         <source>Balance: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Solde: %s</translation>
     </message>
     <message>
         <location filename="cashier.py" line="458"/>
         <source>Send</source>
-        <translation type="unfinished"></translation>
+        <translation>Envoyer</translation>
     </message>
     <message>
         <location filename="cashier.py" line="458"/>
         <source>Send bitcoins to another person</source>
-        <translation type="unfinished"></translation>
+        <translation>Envoyer des bitcoins à une autre personne</translation>
     </message>
     <message>
         <location filename="cashier.py" line="461"/>
         <source>New address</source>
-        <translation type="unfinished"></translation>
+        <translation>Nouvelle adresse</translation>
     </message>
     <message>
         <location filename="cashier.py" line="461"/>
         <source>Create new address for accepting bitcoins</source>
-        <translation type="unfinished"></translation>
+        <translation>Créer une nouvelle adresse pour accepter les bitcoins</translation>
     </message>
     <message>
         <location filename="cashier.py" line="465"/>
         <source>Copy address</source>
-        <translation type="unfinished"></translation>
+        <translation>Copier l&apos;adresse</translation>
     </message>
     <message>
         <location filename="cashier.py" line="465"/>
         <source>Copy address to clipboard</source>
-        <translation type="unfinished"></translation>
+        <translation>Copier l&apos;adresse dans le presse-papier</translation>
     </message>
     <message>
         <location filename="cashier.py" line="469"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Préférences</translation>
     </message>
     <message>
         <location filename="cashier.py" line="469"/>
         <source>Configure Spesmilo</source>
-        <translation type="unfinished"></translation>
+        <translation>Configurer Spesmilo</translation>
     </message>
 </context>
 <context>
@@ -63,17 +63,17 @@
     <message>
         <location filename="main.py" line="56"/>
         <source>Connecting...</source>
-        <translation type="unfinished"></translation>
+        <translation>Connection...</translation>
     </message>
     <message>
         <location filename="main.py" line="45"/>
         <source>&amp;Configure</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Configurer</translation>
     </message>
     <message>
         <location filename="main.py" line="49"/>
         <source>&amp;Abort</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Annuler</translation>
     </message>
 </context>
 <context>
@@ -81,7 +81,7 @@
     <message>
         <location filename="main.py" line="186"/>
         <source>Error connecting</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur lors de la connection</translation>
     </message>
 </context>
 <context>
@@ -89,42 +89,42 @@
     <message>
         <location filename="send.py" line="42"/>
         <source>BTC</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="send.py" line="43"/>
         <source>TBC</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="send.py" line="47"/>
         <source>Pay to:</source>
-        <translation type="unfinished"></translation>
+        <translation>Payable à:</translation>
     </message>
     <message>
         <location filename="send.py" line="52"/>
         <source>Amount:</source>
-        <translation type="unfinished"></translation>
+        <translation>Montant:</translation>
     </message>
     <message>
         <location filename="send.py" line="55"/>
         <source>&amp;Send</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Envoyer</translation>
     </message>
     <message>
         <location filename="send.py" line="58"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Annuler</translation>
     </message>
     <message>
         <location filename="send.py" line="65"/>
         <source>&lt;i&gt;Enter a bitcoin address (e.g. 1A9Pv2PYuZYvfqku7sJxovw99Az72mZ4YH)&lt;/i&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;i&gt;Entrez une adresse bitcoin (par exemple: 1A9Pv2PYuZYvfqku7sJxovw99Az72mZ4YH)&lt;/i&gt;</translation>
     </message>
     <message>
         <location filename="send.py" line="73"/>
         <source>Send bitcoins</source>
-        <translation type="unfinished"></translation>
+        <translation>Envoyer des pièces</translation>
     </message>
     <message>
         <location filename="send.py" line="95"/>
@@ -134,32 +134,32 @@
     <message>
         <location filename="send.py" line="152"/>
         <source>Invalid address</source>
-        <translation type="unfinished"></translation>
+        <translation>Adresse invalide</translation>
     </message>
     <message>
         <location filename="send.py" line="152"/>
         <source>Invalid address: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Adresse invalide : %s</translation>
     </message>
     <message>
         <location filename="send.py" line="165"/>
         <source>Insufficient balance</source>
-        <translation type="unfinished"></translation>
+        <translation>Le montant dépasse votre solde</translation>
     </message>
     <message>
         <location filename="send.py" line="165"/>
         <source>Balance of %g is too small.</source>
-        <translation type="unfinished"></translation>
+        <translation>Le solde %g est trop petit.</translation>
     </message>
     <message>
         <location filename="send.py" line="175"/>
         <source>Error sending</source>
-        <translation type="unfinished"></translation>
+        <translation>Erreur lors de l&apos;envoie</translation>
     </message>
     <message>
         <location filename="send.py" line="175"/>
         <source>Your send failed: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Votre envoie a échoué: %s</translation>
     </message>
 </context>
 <context>
@@ -167,32 +167,32 @@
     <message>
         <location filename="settings.py" line="243"/>
         <source>&amp;OK</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="settings.py" line="248"/>
         <source>&amp;Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Appliquer</translation>
     </message>
     <message>
         <location filename="settings.py" line="253"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Annuler</translation>
     </message>
     <message>
         <location filename="settings.py" line="265"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Préférences</translation>
     </message>
     <message>
         <location filename="settings.py" line="290"/>
         <source>Restart required</source>
-        <translation type="unfinished"></translation>
+        <translation>Redémarrage requis</translation>
     </message>
     <message>
         <location filename="settings.py" line="290"/>
         <source>Restarting Spesmilo is required for some changes to take effect.</source>
-        <translation type="unfinished"></translation>
+        <translation>Vous devez redémarrer Spesmilo afin que les changements soient effectifs.</translation>
     </message>
 </context>
 <context>
@@ -200,12 +200,12 @@
     <message>
         <location filename="settings.py" line="139"/>
         <source>Use internal core</source>
-        <translation type="unfinished"></translation>
+        <translation>Utiliser le coeur interne</translation>
     </message>
     <message>
         <location filename="settings.py" line="144"/>
         <source>URI:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
 </context>
 <context>
@@ -218,22 +218,22 @@
     <message>
         <location filename="settings.py" line="173"/>
         <source>American</source>
-        <translation type="unfinished"></translation>
+        <translation>Americain</translation>
     </message>
     <message>
         <location filename="settings.py" line="174"/>
         <source>English</source>
-        <translation type="unfinished"></translation>
+        <translation>Anglais</translation>
     </message>
     <message>
         <location filename="settings.py" line="175"/>
         <source>Esperanto</source>
-        <translation type="unfinished"></translation>
+        <translation>Espéranto</translation>
     </message>
     <message>
         <location filename="settings.py" line="176"/>
         <source>Dutch</source>
-        <translation type="unfinished"></translation>
+        <translation>Néerlandais</translation>
     </message>
     <message>
         <location filename="settings.py" line="177"/>
@@ -243,27 +243,27 @@
     <message>
         <location filename="settings.py" line="183"/>
         <source>Language:</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="settings.py" line="188"/>
         <source>Assume</source>
-        <translation type="unfinished"></translation>
+        <translation>Assumé</translation>
     </message>
     <message>
         <location filename="settings.py" line="189"/>
         <source>Prefer</source>
-        <translation type="unfinished"></translation>
+        <translation>Préféré</translation>
     </message>
     <message>
         <location filename="settings.py" line="190"/>
         <source>Force</source>
-        <translation type="unfinished"></translation>
+        <translation>Forcé</translation>
     </message>
     <message>
         <location filename="settings.py" line="195"/>
         <source>Decimal</source>
-        <translation type="unfinished"></translation>
+        <translation>Décimal</translation>
     </message>
     <message>
         <location filename="settings.py" line="196"/>
@@ -273,12 +273,12 @@
     <message>
         <location filename="settings.py" line="199"/>
         <source>Number system:</source>
-        <translation type="unfinished"></translation>
+        <translation>Système de numération:</translation>
     </message>
     <message>
         <location filename="settings.py" line="201"/>
         <source>Hide preferred unit name</source>
-        <translation type="unfinished"></translation>
+        <translation>Cacher le nom de l&apos;unité préféré</translation>
     </message>
 </context>
 <context>
@@ -286,62 +286,62 @@
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation>Statut</translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Date</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Transactions</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Credits</source>
-        <translation type="unfinished"></translation>
+        <translation>Crédits</translation>
     </message>
     <message>
         <location filename="cashier.py" line="65"/>
         <source>Balance</source>
-        <translation type="unfinished"></translation>
+        <translation>Solde</translation>
     </message>
     <message>
         <location filename="cashier.py" line="109"/>
         <source>Confirmed (%s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Confirmé (%s)</translation>
     </message>
     <message>
         <location filename="cashier.py" line="111"/>
         <source>Processing... (%s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Traitement... (%s)</translation>
     </message>
     <message>
         <location filename="cashier.py" line="113"/>
         <source>Validating... (%s)</source>
-        <translation type="unfinished"></translation>
+        <translation>Validation... (%s)</translation>
     </message>
     <message>
         <location filename="cashier.py" line="159"/>
         <source>Sent to %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Envoyer à %s</translation>
     </message>
     <message>
         <location filename="cashier.py" line="161"/>
         <source>Received to %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Reçu depuis %s</translation>
     </message>
     <message>
         <location filename="cashier.py" line="163"/>
         <source>Generated</source>
-        <translation type="unfinished"></translation>
+        <translation>Généré</translation>
     </message>
     <message>
         <location filename="cashier.py" line="165"/>
         <source>Moved</source>
-        <translation type="unfinished"></translation>
+        <translation>Déplacé</translation>
     </message>
 </context>
 <context>
@@ -349,17 +349,17 @@
     <message>
         <location filename="main.py" line="85"/>
         <source>&amp;Cashier</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Caissier</translation>
     </message>
     <message>
         <location filename="main.py" line="89"/>
         <source>&amp;Send funds</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Envoyer des fonds</translation>
     </message>
     <message>
         <location filename="main.py" line="94"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Quitter</translation>
     </message>
 </context>
 <context>
@@ -372,22 +372,22 @@
     <message>
         <location filename="main.py" line="244"/>
         <source>Use this caption for the cashier window</source>
-        <translation type="unfinished"></translation>
+        <translation>Utilisé cette légende pour ouvrir la fenêtre du caissier</translation>
     </message>
     <message>
         <location filename="main.py" line="250"/>
         <source>Opens an interactive Python prompt, and enables infinite in-RAM logging</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvre une invite interactive en Python et permet un connection in-ram infinie</translation>
     </message>
     <message>
         <location filename="main.py" line="252"/>
         <source>Use this window icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Utiliser cette icône de fenêtre</translation>
     </message>
     <message>
         <location filename="main.py" line="254"/>
         <source>Opens a dialog to send funds</source>
-        <translation type="unfinished"></translation>
+        <translation>Ouvre un dialogue pour envoer des fonds</translation>
     </message>
 </context>
 </TS>
